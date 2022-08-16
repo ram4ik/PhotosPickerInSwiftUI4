dfab6ca08fc0f56e6fb77b10c394ecff48ab7fc2
Ramill <ramill_ibragimov@icloud.com>
2022-08-16 10:49:27 +0300

    init

--- PhotosPickerInSwiftUI4/ContentView.swift
+++ PhotosPickerInSwiftUI4/ContentView.swift
@@ -6,14 +6,38 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var selectedPhotoData: Data?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            if let selectedPhotoData, let image = UIImage(data: selectedPhotoData) {
+                Image(uiImage: image)
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(12)
+                    .padding()
+            } else {
+                Image(systemName: "photo.circle")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(.gray.opacity(0.5))
+                    .padding()
+            }
+            
+            PhotosPicker("Pick a Photo", selection: $selectedItem, matching: .images)
+                .onChange(of: selectedItem) { newValue in
+                    Task {
+                        if let data = try? await newValue?.loadTransferable(type: Data.self) {
+                            selectedPhotoData = data
+                        }
+                    }
+                }
         }
         .padding()
     }
