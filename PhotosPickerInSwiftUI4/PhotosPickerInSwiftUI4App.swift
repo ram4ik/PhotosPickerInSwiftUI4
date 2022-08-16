e21ff3a44af4dbc86987a199740825e50bc90c3c
Ramill <ramill_ibragimov@icloud.com>
2022-08-16 10:33:54 +0300

    Initial Commit

---
+++ PhotosPickerInSwiftUI4/PhotosPickerInSwiftUI4App.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPickerInSwiftUI4App.swift
+//  PhotosPickerInSwiftUI4
+//
+//  Created by Ramill Ibragimov on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPickerInSwiftUI4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
